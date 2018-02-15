19e1f1aaaaa7bb71ee7153abc0607260e82ec1de
Fernando Pérez <fernandoperez@Fernandos-MacBook-Pro.local>
2018-02-15 13:29:05 -0600

    Image Uploader Basic Styling - No Functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,17 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+  
+  console.log("jquery");
+
+ 
+
+});
+
+
